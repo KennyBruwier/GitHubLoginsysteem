ebb2f38f92612241249f39eaeff4e6eaed83e6be
Tim-Audenaert <tim.audenaert@hotmail.be>
2021-07-16 14:49:10 +0200

    Ik ben tim!

--- GitHubLoginsysteem/Program.cs
+++ GitHubLoginsysteem/Program.cs
@@ -7,6 +7,7 @@ namespace GitHubLoginsysteem
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Ik ben Tim!");
         }
     }
 }
