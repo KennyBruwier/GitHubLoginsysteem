befb4c5134d4d17d5ceadb71b240f8b65f8fa037
katia verachtert <katia.verachtert.scherpendries18@outlook.com>
2021-07-16 14:53:40 +0200

    katia

--- GitHubLoginsysteem/Program.cs
+++ GitHubLoginsysteem/Program.cs
@@ -6,7 +6,7 @@ namespace GitHubLoginsysteem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello ik ben katia!");
         }
     }
 }
